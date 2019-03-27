67cb5e9b3a6b477fd7e37cb99c26b1b57a96cc40
github username <github email address>
2019-03-27 17:30:31 -0400

    trouble figuring out how to get pizza to render in pizzaForm

--- src/App.js
+++ src/App.js
@@ -3,12 +3,39 @@ import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
 class App extends Component {
+  state = {
+    pizzas: [],
+    formPizza: {}
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/pizzas')
+    .then(res => res.json())
+    .then(pizzaData => this.setState({
+      pizzas: pizzaData
+    })
+    )
+  }
+
+  handleEditClick = (key) => {
+    console.log('clicking edit');
+    console.log(key)
+    //1. have access to the pizza id in pizza
+    //2. need to get id from pizza to app in order to pass the correct pizza to the PizzaForm
+    //3. use that id to identify which pizza i've clicked
+  }
+
+
   render() {
+
     return (
       <Fragment>
         <Header/>
         <PizzaForm/>
-        <PizzaList/>
+        <PizzaList
+          pizzas={this.state.pizzas}
+          handleEditClick={this.handleEditClick}
+          />
       </Fragment>
     );
   }
--- src/containers/PizzaList.js
+++ src/containers/PizzaList.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import Pizza from '../components/Pizza'
 class PizzaList extends Component {
 
+  displayPizzas = () => {
+    // console.log(this.props)
+    // console.log(this.props.handleEditClick);
+    return this.props.pizzas.map(pizza => {
+      return <Pizza key={pizza.id} pizza={pizza} editPizza={this.props.handleEditClick} />
+    })
+  }
+
   render() {
     return (
       <table className="table table-striped">
@@ -15,7 +23,7 @@ class PizzaList extends Component {
         </thead>
         <tbody>
           {
-            //render Pizza here
+            this.displayPizzas()
           }
         </tbody>
       </table>
